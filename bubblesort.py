cbbb9f664831a457f881ffe8f2f5bacebd217226
PMDCoding <128332889+PMDCoding@users.noreply.github.com>
2023-03-20 00:32:42 +0000

    Starter code.

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,12 @@
 
+def bubble_sort(any_list:[])->[]:
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(0,len(any_list)-1):
+            if any_list[i] > any_list[i+1]:
+                temp = any_list[i]
+                any_list[i] = any_list[i+1]
+                any_list[i+1] = temp
+                sorted = False
+    return any_list
